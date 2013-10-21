91203a51e3201eee4b44f0e06afffdd70b95e2bd
Prosk8er <prosk8er@gotskillslounge.com>
2013-10-21 03:20:30 -0400

    added migration files

---
+++ ext/prosk8er/steam_mod/migrations/v10x/release_1_0_0.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+* @package migration
+* @copyright (c) 2012 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+*
+*/
+
+namespace prosk8er\steam_mod\migrations\v10x;
+
+class release_1_0_0 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['steam_mod_version']) && version_compare($this->config['steam_mod_version'], '1.0.0', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v310\dev');
+	}
+
+	public function update_data()
+	{
+		return array(
+			'add_columns'        => array(
+				$this->table_prefix . 'users' => array(
+					'user_steam' => array('VCHAR', ''),
+				),
+			),
+
+			array('config.add', array('steam_mod_version', '1.0.0')),
+		);
+	}
+}
---
+++ ext/prosk8er/steam_mod/migrations/v10x/release_1_0_2.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* @package migration
+* @copyright (c) 2012 phpBB Group
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+*
+*/
+
+namespace prosk8er\steam_mod\migrations\v10x;
+
+class release_1_0_2 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return version_compare($this->config['steam_mod_version'], '1.0.2', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\prosk8er\steam_mod\migrations\v10x\release_1_0_1');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.update', array('steam_mod_version', '1.0.2')),
+		);
+	}
+}
