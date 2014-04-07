5f2db19ea5dcf0112924f771f4f410c977b7b96c
Prosk8er <prosk8er@gotskillslounge.com>
2014-04-06 20:18:12 -0400

    remove unneeded language and update if statement

--- ext/prosk8er/steammod/language/en/steam.php
+++ ext/prosk8er/steammod/language/en/steam.php
@@ -24,12 +24,4 @@ $lang = array_merge($lang, array(
 	'STEAM_EXPLAIN'		=> 'Please visit the %sFAQ%s to learn more.',
 	'TOO_LONG_STEAM'	=> 'The Steam name you entered is too long.',
 	'TOO_SHORT_STEAM'	=> 'The Steam name you entered is too short.',
-
-	'STEAM_MOD'			=> 'Steam Mod',
-	'INSTALL_STEAM_MOD'		=> 'Install Steam Mod',
-	'INSTALL_STEAM_MOD_CONFIRM'	=> 'Are you sure you want to install the Steam Mod?',
-	'UPDATE_STEAM_MOD'		=> 'Update Steam Mod',
-	'UPDATE_STEAM_MOD_CONFIRM'	=> 'Are you sure you want to update the Steam Mod?',
-	'UNINSTALL_STEAM_MOD'		=> 'Uninstall Steam Mod',
-	'UNINSTALL_STEAM_MOD_CONFIRM'	=> 'Are you sure you want to uninstall the Steam Mod?',
 ));
