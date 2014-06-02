ee67b1d8e77f71b836fbb0346d3edd40bc0c16b7
Prosk8er <prosk8er@gotskillslounge.com>
2014-06-02 10:16:16 -0400

    update

---
+++ ext/prosk8er/steamcpf/event/listener.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+*
+* @package Steam CPF Extension
+* @copyright (c) 2014 Prosk8er
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace prosk8er\steamcpf\event;
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* Event listener
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class listener implements EventSubscriberInterface
+{
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.user_setup'						=> 'load_language_on_setup',
+			'core.generate_profile_fields_template_data'		=> 'profile_fields_template_data',
+		);
+	}
+
+	public function setup($event)
+	{
+		global $config;
+	}
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'prosk8er/steamcpf',
+			'lang_set' => 'steam',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	public function profile_fields_template_data($event)
+	{
+		global $template;
+
+		$tpl_fields = array(
+			'STEAM_IS_INT'	=> (!empty($member['user_steam']) && is_numeric($member['user_steam'])) ? true : false,
+		);
+
+		$template->assign_vars($tpl_fields);
+	}
+}
