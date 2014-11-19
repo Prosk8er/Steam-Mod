17f13ef8b63d66e9ae124dbfd4bcfcc6ec01cf07
Prosk8er <prosk8er@gotskillslounge.com>
2014-11-19 18:59:01 -0500

    couple small updates

--- ext/prosk8er/steamcpf/event/listener.php
+++ ext/prosk8er/steamcpf/event/listener.php
@@ -10,32 +10,21 @@
 
 namespace prosk8er\steamcpf\event;
 
-/**
-* @ignore
-*/
-if (!defined('IN_PHPBB'))
-{
-	exit;
-}
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
 * Event listener
 */
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-
 class listener implements EventSubscriberInterface
 {
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.user_setup'						=> 'load_language_on_setup',
-			'core.generate_profile_fields_template_data'		=> 'profile_fields_template_data',
+			'core.user_setup'					=> 'load_language_on_setup',
+			'core.memberlist_view_profile'		=> 'memberlist_view_profile',
+			'core.viewtopic_modify_post_row'	=> 'viewtopic_modify_post_row',
+			'core.ucp_pm_view_messsage'			=> '',
 		);
-	}
-
-	public function setup($event)
-	{
-		global $config;
 	}
 
 	public function load_language_on_setup($event)
@@ -48,14 +37,24 @@ class listener implements EventSubscriberInterface
 		$event['lang_set_ext'] = $lang_set_ext;
 	}
 
-	public function profile_fields_template_data($event)
+	public function memberlist_view_profile($event)
 	{
-		global $template;
+		$profile_fields = array();
+		if ($config['load_cpf_viewprofile'])
+		{
+			$cp = $phpbb_container->get('profilefields.manager');
+			$profile_fields = $cp->grab_profile_fields_data($user_id);
+			'STEAM_IS_INT'	=> (!empty($profile_fields['pf_phpbb_steam']) && is_numeric($profile_fields['pf_phpbb_steam'])) ? true : false,);
+		{
+		$vars = array('profile_fields',);
+	}
 
-		$tpl_fields = array(
-			'STEAM_IS_INT'	=> (!empty($member['user_steam']) && is_numeric($member['user_steam'])) ? true : false,
+	public function viewtopic_modify_post_row($event)
+	{
+		$cp_row = array();
+		$post_row = array(
+			'STEAM_IS_INT'			=> (!empty($cp_row['pf_phpbb_steam']) && is_numeric($cp_row['pf_phpbb_steam'])) ? true : false,);
 		);
-
-		$template->assign_vars($tpl_fields);
+		$vars = array('row', 'cp_row',);
 	}
 }
